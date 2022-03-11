efd9c2bd723e3c22bc4efc07b2b25ad497660d41
Kalebtes2031 <kalebtesfaye2031@gmail.com>
2022-03-11 02:22:35 -0800

    print all single digits of base 10 with out char

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+/**
+  * main - print all single digit numbers of base 10 without char
+  *
+  * Return: zero
+  */
+
+int main (void)
+{
+	int num = 0;
+	for ( num= 0; num <= 9; num++)
+	{
+		putchar(num);
+	} 
+	putchar('\n');
+  return (0);
+} 
