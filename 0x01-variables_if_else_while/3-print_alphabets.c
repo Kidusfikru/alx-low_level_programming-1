e9eae8a485cb721d670c1b4b4087676a591887c7
Kalebtes2031 <kalebtesfaye2031@gmail.com>
2022-03-11 04:16:51 -0800

    print lowercase and uppercase alphabet

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -2,22 +2,23 @@
 
 /**
   * main - print alphabet in lowercase and then in uppercase
-  * 
+  *
   * Return: zero
   */
 
-int main (void)
+int main(void)
 {
 	char ch1 = 'a';
 	char ch2 = 'A';
-	for ( ch1= 'a'; ch1 <= 'z'; ch1++)
+	
+	for (ch1 = 'a'; ch1 <= 'z'; ch1++)
 	{
 		putchar(ch1);
-	} 
-	for ( ch2= 'A'; ch2 <= 'Z'; ch2++)
+	}
+	for (ch2 = 'A'; ch2 <= 'Z'; ch2++)
 	{
 		putchar(ch2);
-	} 
+	}
 	putchar('\n');
-  return (0);
-} 
+	return (0);
+}
