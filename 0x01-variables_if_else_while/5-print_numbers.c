b13de66d7adc5b7a7d5fd1574658c9c2ac2c77e3
Kalebtes2031 <kalebtesfaye2031@gmail.com>
2022-03-11 12:00:15 -0800

    print all single digits of base 10

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -2,17 +2,18 @@
 
 /**
   * main - print all single digit numbers of base 10 starting from 0
-  * 
+  *
   * Return: zero
   */
 
-int main (void)
+int main(void)
 {
 	int num = 0;
-	for ( num= 0; num <= 9; num++)
+
+	for (num = 0; num <= 9; num++)
 	{
 		printf("%d", num);
-	} 
+	}
 	printf("\n");
-  return (0);
-} 
+	return (0);
+}
