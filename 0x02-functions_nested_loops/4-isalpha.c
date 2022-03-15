960895a1f75f29a888cb5c873e2bc0a4dcd06235
Kalebtes2031 <kalebtesfaye2031@gmail.com>
2022-03-15 12:05:21 +0300

    checks whether it is alphabet

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * _isalpha - check for alphabetic character.
- *
- * @c: parameter
- * Return: Always 0.
- */
+  * _isalpha - check for alphabetic character.
+  *
+  * @c: parameter
+  * Return: Always 0.
+  */
 int _isalpha(int c)
 {
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
