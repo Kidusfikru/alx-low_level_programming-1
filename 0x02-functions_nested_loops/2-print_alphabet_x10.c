209fc762a01bc630d28bffcd4b9d3f794632b2a6
Kalebtes2031 <kalebtesfaye2031@gmail.com>
2022-03-16 01:41:04 +0300

    printing lowercase alphabets X10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -11,13 +11,13 @@ void print_alphabet_x10(void)
 	int counter = 1;
 	int i;
 
-	while (counter <=10)
+	while (counter <= 10)
 	{
 		for (i = 'a'; i <= 'z'; i++)
 		{
 			_putchar(i);
 		}
 		_putchar('\n');
-		counter ++;
+		counter++;
 	}
 }
