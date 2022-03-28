c8bd69a6f4defd934028a4b22bdcbafc55f48162
Kalebtes2031 <kalebtesfaye2031@gmail.com>
2022-03-28 23:05:40 +0300

    searches a string for any of a set of byte

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,18 +1,22 @@
 #include "main.h"
 /**
-  * _strchr - locates a character in a string,
-  * @s: string.
-  * @c: character.
-  * Return: the pointer to the first occurrence of the character c.
+  * _strpbrk - searches a string for any of a set of bytes.
+  * @s: first string.
+  * @accept: second string.
+  * Return: a pointer to the byte in s that matches one of the
+  * bytes in accept, or NULL if no such byte is found.
   */
-char *_strchr(char *s, char c)
+char *_strpbrk(char *s, char *accept)
 {
-	unsigned int i = 0;
+	unsigned int i, j;
 
-	for (; *(s + i) != '\0'; i++)
-		if (*(s + i) == c)
-			return (s + i);
-	if (*(s + i) == c)
-		return (s + i);
+	for (i = 0; *(s + i) != '\0'; i++)
+	{
+		for (j = 0; *(accept + j) != '\0'; j++)
+		{
+			if (*(s + i) == *(accept + j))
+				return (s + i);
+		}
+	}
 	return ('\0');
 }
